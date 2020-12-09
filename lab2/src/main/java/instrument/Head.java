33991bcacd223201f065901ac2de0a41c491ce8e
toliksadovnichiy <60927422+toliksadovnichiy@users.noreply.github.com>
2020-12-09 22:15:27 +0200

    Add files via upload

---
+++ lab2/src/main/java/instrument/Head.java
@@ -0,0 +1,36 @@
+package instrument;
+
+public class Head {
+    private Color color;
+    private Tuners tuners;
+
+    public Head(){
+        this.color = Guitar.randomColor();
+        this.tuners = new Tuners();
+    }
+    public Head(Tuners tuners){
+        this.color = Guitar.randomColor();
+        this.tuners = tuners;
+    }
+
+    public String tuneTheGuitar(){
+        System.out.println("Now the guitar will be tuned.\n");
+        return tuners.turnTuners();
+    }
+
+    public void display() {
+        System.out.println("-------------Head-------------");
+        System.out.println("Color: " + color);
+        tuners.display();
+    }
+
+    public Color getColor() { return color; }
+
+    public Tuners getTuners() { return tuners; }
+
+    public void setColor(Color color) { this.color = color; }
+
+    public void setTuners(Tuners tuners) { this.tuners = tuners; }
+
+
+}
---
+++ lab2/src/main/java/instrument/Tuners.java
@@ -0,0 +1,41 @@
+package instrument;
+
+public class Tuners {
+    private int quantity;
+    private Material material;
+
+    public Tuners(){
+        this.quantity = Guitar.randomNumber(4, 12, 2);
+        this.material = Guitar.randomMaterial();
+    }
+
+    public String turnTuners(){
+        for(int i=0; i<quantity; i++){
+            System.out.println("Turn " + i + " tuner.");
+        }
+        return "\nGuitar was tuned.";
+    }
+
+    public void display() {
+        System.out.println("Tuners quantity: " + quantity);
+        System.out.println("Tuners material: " + material);
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+
+}
